676fd2bac2664b69c841801026d524e948462b21
asaad227 <a.saad227@yahoo.com>
2022-01-25 14:44:39 +0000

    padding added on the input field

--- src/components/InputList/index.js
+++ src/components/InputList/index.js
@@ -22,7 +22,7 @@ function InputList({ addToList, buttonText }) {
 
   return (
     <section>
-      <input value={input} onChange={handleInput} />
+      <input style={{"padding-left": "15px"}} value={input} onChange={handleInput} />
       <button className="InputList" onClick={handleClick}>
         {buttonText}
       </button>
